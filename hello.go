5ba37661ea0f2967b571eb750b00bad39250332a
carlos olimpio barros carneiro junior <carlos.ojunior00@gmail.com>
2025-04-12 17:48:38 -0400

    aprendendo variávis

--- hello.go
+++ hello.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello word")
-	println("hello word 2")
+	var nome string = "Carlos"
+	var versao float32 = 1.1
+	var idade int = 28
+	fmt.Println("hello word,", nome)
+	fmt.Println("este programa está na versão", versao)
+	fmt.Println("sua idade é", idade)
 }
