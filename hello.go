3c45b22e809164ad5c1881f2315216e0106c54e1
carlos olimpio barros carneiro junior <carlos.ojunior00@gmail.com>
2025-04-12 18:10:37 -0400

    aprendendo sofre typeof e inferencia

--- hello.go
+++ hello.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var nome string = "Carlos"
-	var versao float32 = 1.1
-	var idade int = 28
+	nome := "Carlos"
+	versao := 1.1
+	idade := 28
 	fmt.Println("hello word,", nome)
 	fmt.Println("este programa está na versão", versao)
 	fmt.Println("sua idade é", idade)
+	fmt.Println("o tipo da variável nome é:", reflect.TypeOf(nome))
 }
